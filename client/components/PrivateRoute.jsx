3050bda8342936516d4c8356b695458b3a2d48b7
ganguls <110716400+ganguls@users.noreply.github.com>
2024-05-20 11:17:45 +0530

    Delete client/components directory

--- client/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import {useSelector} from 'react-redux'
-import {Outlet,Navigate} from 'react-router-dom'
-
-export default function PrivateRoute() {
-    const {curruntUser} = useSelector((state)=>state.user)
-
-  return curruntUser ? <Outlet />:<Navigate to='/signin' />;
-}
